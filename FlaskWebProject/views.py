e756182d36781389c9e9024462060816fb4c94b5
Pratira <pratira.zadoo@mavs.uta.edu>
2016-07-21 23:26:45 -0500

    new app

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,6 +1,3 @@
-"""
-Routes and views for the flask application.
-"""
 import datetime
 from pymongo import MongoClient
 from datetime import datetime
@@ -25,24 +22,26 @@ def welcome():
 def new_user():
     username = request.form['new_user']
     password = request.form['new_pass']
+    quota = request.form['quota']
+    size = request.form['size']
     item_dir = {
         'username': username,
         'password': password,
+        'quota' : quota,
+        'size' : size
     }
     print "New Values taken"
     
-    result = db.pzdb.users.find({'password':password,'username':username}).count()>0
+    result = db.pzdb.find({'password':password,'username':username}).count()>0
     
     if not result:
         print "inserts value in db"
-        db.pzdb.users.insert(item_dir)
+        db.pzdb.insert(item_dir)
     else:
         return 'User Already Exists!'
     
     return render_template('login.html') 
     #print ("Successfully logged in!")
-
-
 
 @app.route('/user_login',methods=['GET','POST'])
 def login():
@@ -54,7 +53,7 @@ def login():
     }
     global userID
     userID = username
-    res = db.pzdb.users.find({'username': username , 'password': password}).count() > 0
+    res = db.pzdb.find({'username': username , 'password': password}).count() > 0
     if res :
         return render_template('upload.html', user = userID)
     else:
@@ -62,70 +61,358 @@ def login():
 
     return render_template('upload.html', user = userID)
 
-
-@app.route('/flight', methods=['POST'])
-def flight():
-    hub = request.form['hub']
-    vacation = request.form['vacation']
-    traveldate = request.form['date_of_travel']
-
-    print hub
-    print vacation
-    print traveldate
-
+@app.route('/upload', methods=['POST'])
+def upload():
+    print "Inside upload photo"
+    subject = request.form['subject']
+    #input subject and split
+    splitSubject = subject.split(' ')
+    if len(splitSubject) > 1:
+        return render_template("upload.html", user = userID, errormsg = "Please enter the single word subject" )
+    #input filename and priority
+    filename = request.form['filename']
+    priority = request.form['priority']
+    #calculate time to upload
+    time_to_upload = datetime.now().strftime("%Y-%m-%d %H:%M") 
+    #browse and select file from the local machine
+    file = request.files['filecontents']
+    #target stores and reads the file
+    target = file.read()
+    #length of file gets calculated
+    length_of_file = len(target)
+    #check the content type of file
+    contentType = file.content_type
+    print file.filename
+    #define fs
     fs = gridfs.GridFS(db)
     print 'gridFS'
     
-    #stored = fs.put(username=userID, hub=hub, vacation=vacation,date_of_travel=traveldate)
-    #print stored
+    #file saved with contents in target, filename, username and priority on the gridFS
+    stored = fs.put(target, filename=filename, username=userID, priority = priority, content_type = contentType)
+    print stored
     fs = gridfs.GridFS(db)
+    print 'file length' + str(length_of_file)
     res = db.register.find({'username':userID})    
-        
+    #calculating the quota and size remaining with the user
+    quota = ''
+    size = ''
+    for item in res:
+        print item['size']
+        filesize = item['size']
+        quota = item['quota']
+        break;
     
+    count = 0
+    total_length = 0
+    for item in db.fs.files.find({"username":userID}):
+        count = count+1
+        length_of_file = length_of_file + item['length']
+    
+    if count >= quota:
+        return 'User Quota to upload file is exceeded'
+    
+    if size < length_of_file:
+        return 'User file size quota to upload file is exceeded the specified limit of : ' + size + 'bytes'
+    
+    #appending items with all the file contents
     fs = gridfs.GridFS(db)
-    list_allFlights  = [] 
-    for file in db.reservations.find():
-        
-        traveldate = file['date']
-        print traveldate
-        source = file['vacation']
-        print source
-        
-        if file['date'] == traveldate:
-            if file['hub'] == hub:
-                if file['vacation'] == vacation:
-                    contents = {}
-                    contents['date']=file['date']
-                    contents['name']=file['name']
-                    contents['hub']=file['hub']
-                    contents['vacation']=file['vacation']
-                    list_allFlights.append(contents)
-                    print contents   
+    items = []
 
-    #return 'here'
-    return render_template("displayPhotos.html",items=list_allFlights, userID = userID)
+    for item in db.fs.files.find({"username":userID}):
+        filename = item['filename']
+        read_file = fs.find_one({"filename": filename}).read()
+        #condition checking for text files
+        if item['contentType'] != 'text/plain':
+            data_received = "data:image/jpeg;base64," + base64.b64encode(read_file)
+            contents = {}
+            contents['filename'] = filename
+            contents['url'] = data_received
+            #adding the upload date in the contents
+            contents['time'] = item['uploadDate']
+            timeSplit = str(contents['time']).split('.')
+            contents['time'] = timeSplit[0]
+            contents['priority'] = item['priority']
+            contents['content_type'] = item['contentType']
+            items.append(contents)
+        else:
+            print 'Inside text content'
+            print read_file
+            contents = {}
+            contents['filename'] = filename
+            contents['content'] = read_file
+            #adding upload date in the contents
+            contents['time'] = item['uploadDate']
+            timeSplit = str(contents['time']).split('.')
+            contents['time'] = timeSplit[0]
+            contents['priority'] = item['priority']
+            contents['content_type'] = item['contentType']
+            items.append(contents)
+            print contents['content']                
 
+    return render_template("displayPhotos.html",items=items, userID = userID, timeup = time_to_upload)
+    """
+    #only for images
+    for item in db.fs.filedb.find({'username': username}):
+        #com = None
+        filename = item['filename']
+        picture = fs.filedb.find_one({'filename':filename}).read()
+        data_received = "data:image/jpeg;base64," + base64.b64.b64encode(picture)
+        dir_var = {}
 
-@app.route('/enterdate', methods=['GET','POST'])
-def showAllFlights():
-    traveldate = request.form['enterdate']
+        dir_var['user'] = username
+        dir_var['filename'] = filename
+        dir_var['file'] = data_received
+
+        item_dir.append(dir_var)
+    print timeUpload
+    return render_template('displayPhotos.html', items = item_dir, now = timeUpload)
+"""
+@app.route('/showAllFiles', methods=['GET','POST'])
+def showAllFiles():
     fs = gridfs.GridFS(db)
     #the array will later on get appended with contents
-    list_allFlights = []
+    list_allFiles = []
     #time to show the entire table
-    #time_to_show = datetime.now().strftime("%Y-%m-%d %H:%M")
-    
-    for file in db.reservations.find():
-        #traveldate = file['date']
-        #read_file = fs.find_one({"filename": filename}).read()
+    time_to_show = datetime.now().strftime("%Y-%m-%d %H:%M")
+    for file in db.fs.files.find():
+        filename = file['filename']
+        read_file = fs.find_one({"filename": filename}).read()
         #checking file type here...first one checks for image and the second one checks for text and stores contents accordingly
-        if file['date'] == traveldate:
+        if file['contentType'] != 'text/plain':
+            data_received="data:image/jpeg;base64," + base64.b64encode(read_file)
             contents = {}
-            contents['date']=file['date']
-            contents['name']=file['name']
-            contents['hub']=file['hub']
-            contents['vacation']=file['vacation']
-            list_allFlights.append(contents)
-            print contents
+            contents['filename']=filename
+            contents['url']=data_received
+            contents['time']=file['uploadDate']
+            timeSplit = str(contents['time']).split('.')
+            contents['time']=timeSplit[0]
+            contents['priority']=file['priority']
+            contents['content_type']=file['contentType']
+            list_allFiles.append(contents)
+        else:
+            contents = {}
+            contents['filename'] = filename
+            contents['content'] = read_file
+            #adding upload date in the contents
+            contents['time'] = file['uploadDate']
+            timeSplit = str(contents['time']).split('.')
+            contents['time'] = timeSplit[0]
+            contents['priority'] = file['priority']
+            contents['content_type'] = file['contentType']
+            list_allFiles.append(contents)
+            print contents['content']
 
-    return render_template("displayPhotos.html",items=list_allFlights, user = userID)
+    return render_template("displayPhotos.html",items=list_allFiles, user = userID, timeup= time_to_show)
+    
+@app.route('/deleteFile', methods=['GET','POST'])
+def delete():
+    filename = request.form['filename']
+
+    fs = gridfs.GridFS(db)
+    
+    #time to delete the file from the fs.file and fs.chunks
+    time_to_delete = datetime.now().strftime("%Y-%m-%d %H:%M")
+    
+    #fetching the username and filename and then remove the file and it's respective chunks from mlabs(backend)
+    for file in db.fs.files.find({"username":userID, "filename" : filename}):
+        #print file['_id']
+        removechunk = db.fs.chunk.remove({"files_id": file['_id']})
+        removefiles = db.fs.files.remove({"username":userID, "filename" : filename})
+    
+    list_allFiles = []
+    for file in db.fs.files.find():
+        filename = file['filename']
+        read_file = fs.find_one({"filename": filename}).read()
+        #checking if file type is image/jpeg (converting to base64 and uploading it then)
+        if file['contentType'] != 'text/plain':
+            data_received="data:image/jpeg;base64," + base64.b64encode(read_file)
+            contents = {}
+            contents['filename']=filename
+            contents['url']=data_received
+            contents['time']=file['uploadDate']
+            timeSplit = str(contents['time']).split('.')
+            contents['time']=timeSplit[0]
+            contents['priority']=file['priority']
+            contents['content_type']=file['contentType']
+            list_allFiles.append(contents)
+        else:
+            #checking if file type is text/plain
+            contents = {}
+            contents['filename'] = filename
+            contents['content'] = read_file
+            #adding upload date in the contents
+            contents['time'] = file['uploadDate']
+            timeSplit = str(contents['time']).split('.')
+            contents['time'] = timeSplit[0]
+            contents['priority'] = file['priority']
+            contents['content_type'] = file['contentType']
+            list_allFiles.append(contents)
+
+    return render_template("displayPhotos.html",items=list_allFiles, user = userID, timeup = time_to_delete)
+
+@app.route('/searchFile', methods=['POST'])
+def searchFile():
+
+    global userID
+
+    #taking the string input by the user on the form
+    criteria = request.form['searchFile']
+    #splitting the input on the basis of blank spaces
+    
+    #time to search
+    time_to_search = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+    criteria = criteria.split(" ")
+    print criteria
+    #split into field
+    field = criteria[0]
+    #split into comparison operator
+    comparison = criteria[1]
+    #split into value
+    value = criteria[2]
+    print field
+    print comparison
+    print value
+
+    #assigning the equivalent comparison operator 
+    if comparison == '<':
+        comparison = "$lt"
+    elif comparison == '>':
+        comparison = "$gt"
+    else:
+        comparison = "$eq"
+
+    if field == "date":
+        field = "time"
+        value = str(datetime.strptime(str(value), '%Y-%m-%dT%H:%M:%SZ'))
+        print type(value)
+    elif field== "priority":
+        value = int(value)
+        print 'after priority field'
+    
+    fs = gridfs.GridFS(db)
+
+    listAllFiles = []
+    print field
+    print comparison
+    print value
+    #v=str(value)
+    searchAllFiles = {"username":userID, field:{comparison:str(value)}}
+    print str(searchAllFiles)
+
+    for file in db.fs.files.find(searchAllFiles):
+        filename = file['filename']
+        print filename
+        read_file = fs.find_one({"filename": filename}).read()
+        print read_file
+        if file['contentType'] != 'text/plain':
+            data_received="data:image/jpeg;base64," + base64.b64encode(read_file)
+            contents = {}
+            contents['filename']=filename
+            contents['url']=data_received
+            contents['time']=file['uploadDate']
+            timeSplit = str(contents['time']).split('.')
+            contents['time']=timeSplit[0]
+            contents['priority']=file['priority']
+            contents['content_type']=file['contentType']
+            listAllFiles.append(contents)
+            print 'end of if condition 1'
+        else:
+            print 'inside second if condition of text'
+            contents = {}
+            contents['filename'] = filename
+            contents['content'] = read_file
+            #adding upload date in the contents
+            contents['time'] = file['uploadDate']
+            timeSplit = str(contents['time']).split('.')
+            contents['time'] = timeSplit[0]
+            contents['priority'] = file['priority']
+            contents['content_type'] = file['contentType']
+            listAllFiles.append(contents)
+            print 'end of if condition 2'
+    print 'outside of loop'
+    return render_template("displayPhotos.html", items=listAllFiles, user=userID)
+
+@app.route('/updatePriority', methods=['POST','GET'])
+def update():
+    priority = request.form['priority']
+    filename = request.form['filename']
+    
+    fs = gridfs.GridFS(db)
+
+    db.fs.files.find_and_modify(query={'filename':filename},update={'$set':{'priority' : priority}})
+
+    updateFiles = []
+    for file in db.fs.files.find({"username":userID}):
+        filename = file['filename']
+        #print filename
+        read_file = fs.find_one({"filename": filename}).read()
+        #print read_file
+        if file['contentType'] != 'text/plain':
+            data_received="data:image/jpeg;base64," + base64.b64encode(read_file)
+            contents = {}
+            contents['filename']=filename
+            contents['url']=data_received
+            contents['time']=file['uploadDate']
+            timeSplit = str(contents['time']).split('.')
+            contents['time']=timeSplit[0]
+            contents['priority']=file['priority']
+            contents['content_type']=file['contentType']
+            updateFiles.append(contents)
+            #print 'end of if condition 1'
+        else:
+            print 'inside second if condition of text'
+            contents = {}
+            contents['filename'] = filename
+            contents['content'] = read_file
+            #adding upload date in the contents
+            contents['time'] = file['uploadDate']
+            timeSplit = str(contents['time']).split('.')
+            contents['time'] = timeSplit[0]
+            contents['priority'] = file['priority']
+            contents['content_type'] = file['contentType']
+            updateFiles.append(contents)
+            #print 'end of if condition 2'
+    #print 'outside of loop'
+    return render_template("displayPhotos.html", items=updateFiles,user=userID)
+
+@app.route('/sort', methods=['POST', 'GET'])
+def sort():
+    field = str(request.form['field'])
+    order = int(request.form['order'])
+    print order
+    print field
+
+    fs = gridfs.GridFS(db)
+
+    sortFiles = []
+
+    for file in db.fs.files.find().sort([(field , order)]):
+        filename = file['filename']
+        read_file = fs.find_one({"filename": filename}).read()
+        if file['contentType'] != 'text/plain':
+            data_received="data:image/jpeg;base64," + base64.b64encode(read_file)
+            contents = {}
+            contents['filename']=filename
+            contents['url']=data_received
+            contents['time']=file['uploadDate']
+            timeSplit = str(contents['time']).split('.')
+            contents['time']=timeSplit[0]
+            contents['priority']=file['priority']
+            contents['content_type']=file['contentType']
+            sortFiles.append(contents)
+            #print 'end of if condition 1'
+        else:
+            print 'inside second if condition of text'
+            contents = {}
+            contents['filename'] = filename
+            contents['content'] = read_file
+            #adding upload date in the contents
+            contents['time'] = file['uploadDate']
+            timeSplit = str(contents['time']).split('.')
+            contents['time'] = timeSplit[0]
+            contents['priority'] = file['priority']
+            contents['content_type'] = file['contentType']
+            sortFiles.append(contents)
+
+    return render_template("displayPhotos.html", items=sortFiles,user=userID)
